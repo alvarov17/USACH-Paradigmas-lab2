9c6e9717f49488345936666c66642251c198f220
Alvaro Valenzuela <alvaro.valenzuela.alfaro@gmail.com>
2022-10-20 21:49:12 -0300

    avances

--- TDA_image_191118898_AlvaroValenzuela.pl
+++ TDA_image_191118898_AlvaroValenzuela.pl
@@ -36,3 +36,22 @@ imageIsPixmap(I) :-
 imageIsHexmap(I) :-
   image(_, _, Pixels, I),
   pixelsArePixrgb(Pixels). 
+
+
+pixelFlipH(Width, PixelIn, PixelOut) :-
+  pixbit(X, Y, Bit, Depth, PixelIn),
+  ( Y =< Width - 1 -> NewY = Y - 1 ; NewY = Y + 1),
+  pixbit(X, NewY, Bit, Depth, PixelOut).
+
+
+pixelsFlipH(_, [], []).
+pixelsFlipH(Width, [PixelIn | PixelsIn], [PixelOut | PixelsOut]) :-
+  pixelFlipH(Width, PixelIn, PixelOut),
+  pixelsFlipH(Width, PixelsIn, PixelsOut).
+
+
+imageFlipH(ImageIn, ImageOut) :-
+  image(Width, _, PixelsIn, ImageIn),
+  pixelsFlipH(Width, PixelsIn, PixelsOut),
+  image(_, _, PixelsOut, ImageOut).
+
