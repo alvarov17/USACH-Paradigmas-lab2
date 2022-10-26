138d99e85d93a22a2e4a8b8277585d155479e022
Alvaro Valenzuela <alvaro.valenzuela.alfaro@gmail.com>
2022-10-25 22:38:29 -0300

    crop

--- TDA_image_191118898_AlvaroValenzuela.pl
+++ TDA_image_191118898_AlvaroValenzuela.pl
@@ -1,3 +1,18 @@
+/* TDA PIXBIT
+
+Dominio:
+  X: Int
+  T: Int
+  Bit: Int {0, 1}
+  Depth: Int{0, 1 ... 255}
+  pixbit: List
+
+Predicados:
+  pixbit(X, Y, Bit, Depth, [X, Y, Bit, Depth]) (Aridad = 5)
+
+*/
+
+% Constructor y Pertenencia
 pixbit(X, Y, Bit, Depth, [X, Y, Bit, Depth]) :- Bit >= 0, Bit =< 1.
 
 pixrgb(X, Y, Red, Green, Blue, Depth, [X, Y, Red, Green, Blue, Depth]) :- 
@@ -37,11 +52,15 @@ imageIsHexmap(I) :-
   image(_, _, Pixels, I),
   pixelsArePixrgb(Pixels). 
 
-
 pixelFlipH(Width, PixelIn, PixelOut) :-
   pixbit(X, Y, Bit, Depth, PixelIn),
-  ( Y =< Width - 1 -> NewY = Y - 1 ; NewY = Y + 1),
+  ( Y is Width - 1 -> NewY is Y - 1 ; NewY is Y + 1),
   pixbit(X, NewY, Bit, Depth, PixelOut).
+
+pixelFlipV(Height, PixelIn, PixelOut) :-
+  pixbit(X, Y, Bit, Depth, PixelIn),
+  ( X is Height - 1 -> NewX is X - 1 ; NewX is X + 1),
+  pixbit(Y, NewX, Bit, Depth, PixelOut).
 
 
 pixelsFlipH(_, [], []).
@@ -49,9 +68,38 @@ pixelsFlipH(Width, [PixelIn | PixelsIn], [PixelOut | PixelsOut]) :-
   pixelFlipH(Width, PixelIn, PixelOut),
   pixelsFlipH(Width, PixelsIn, PixelsOut).
 
+pixelsFlipV(_, [], []).
+pixelsFlipV(Height, [PixelIn | PixelsIn], [PixelOut | PixelsOut]) :-
+  pixelFlipV(Height, PixelIn, PixelOut),
+  pixelsFlipV(Height, PixelsIn, PixelsOut).
+
 
 imageFlipH(ImageIn, ImageOut) :-
   image(Width, _, PixelsIn, ImageIn),
   pixelsFlipH(Width, PixelsIn, PixelsOut),
   image(_, _, PixelsOut, ImageOut).
+
+
+imageFlipV(ImageIn, ImageOut) :-
+  image(_, Height, PixelsIn, ImageIn),
+  pixelsFlipV(Height, PixelsIn, PixelsOut),
+  image(_, _, PixelsOut, ImageOut).
+
+cropPixel(PixelIn, X1, Y1, X2, Y2, PixelOut) :-
+    pixbit(X, Y, Bit, Depth, PixelIn), 
+    (   X >= X1,
+    	X =< X2,
+    	Y >= Y1,
+    	Y =< Y2 ->  pixbit(X, Y, Bit, Depth, PixelOut) ; true).
+
+cropPixels([], _, _, _, _, []).
+cropPixels([PixelIn | PixelsIn], X1, Y1, X2, Y2, [PixelOut | PixelsOut]) :-
+    cropPixel(PixelIn, X1, Y1, X2, Y2, PixelOut),
+    cropPixels(PixelsIn, X1, Y1, X2, Y2, PixelsOut).
+
+imageCrop(ImageIn, X1, Y1, X2, Y2, ImageOut) :-
+    image(Width, Height, PixelsIn, ImageIn),
+    cropPixels(PixelsIn, X1, Y1, X2, Y2, PixelsOut),
+    image(Width, Height, PixelsOut, ImageOut).
+    
 
